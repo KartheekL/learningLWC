a97228db3dc343ff041765ff6ba8be0a64570351
Kartheek Lingampet <kartheekl228@gmail.com>
2021-01-12 21:42:07 +0530

    child parent changes commit.

--- force-app/main/default/lwc/child/child.js
+++ force-app/main/default/lwc/child/child.js
@@ -1,3 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class Child extends LightningElement {}
+export default class Child extends LightningElement {
+	data = [];
+	info = {};
+	@api
+	get selectedRows() {
+		return [];
+	}
+	set selectedRows(values) {
+		this.info.selectedRows = values;
+		this.selectRows(values);
+	}
+	@api
+	get rows() {
+		return [];
+	}
+	set rows(values) {
+		this.info.totalRows = values;
+		this.createData(values);
+	}
+	createData(values) {
+		values.forEach((element) => {
+			this.data.push({
+				id: element,
+				value: element,
+				status: "slds-text-body_small"
+			});
+		});
+	}
+	selectRows(values) {
+		values.forEach((element) => {
+			this.data[element - 1].status = "slds-text-heading_large";
+		});
+	}
+}
---
+++ force-app/main/default/lwc/parent/parent.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+
+export default class Parent extends LightningElement {
+	rows = [1, 2, 3];
+	selectedRows = [2];
+}
